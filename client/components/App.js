99602d4f795a8263d0e912c75e2ffdc46fe485ae
Jason Deng <jdeng1234@gmail.com>
2016-08-09 22:17:18 -0400

    added hot reloading

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default () => {
-	return (
-		<h1> Hello World from React</h1>
-	)
+class App extends React.Component {
+	render() {
+		return (
+			<h1> Hello World from React</h1>
+		);
+	}
 }
+
+export default App;
